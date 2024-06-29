ffacecb8d2f6d7411066f026f9df42f5c950ba26
Luca Domeneghetti <31939791+lucadomene@users.noreply.github.com>
2024-06-29 09:25:00 +0200

    Aggiunto test base

---
+++ src/test/java/it/unife/cavicchidome/CircoloCulturale/BasicTest.java
@@ -0,0 +1,12 @@
+package it.unife.cavicchidome.CircoloCulturale;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BasicTest {
+
+    @Test
+    void FirstTest() {
+        assertThat(1).isEqualTo(1);
+    }
+}
