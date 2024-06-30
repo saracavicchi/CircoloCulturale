a87d1a1e4d7186ff8cbeb764f56e9c4cd4f52f55
saracavicchi <sara02.cavicchi@edu.unife.it>
2024-06-30 19:19:15 +0200

    Aggiunta Tessera model e service. Spostamento metodi di createUtente e socio e di validazione

--- src/main/java/it/unife/cavicchidome/CircoloCulturale/controllers/AuthController.java
+++ src/main/java/it/unife/cavicchidome/CircoloCulturale/controllers/AuthController.java
@@ -3,6 +3,15 @@ package it.unife.cavicchidome.CircoloCulturale.controllers;
 import it.unife.cavicchidome.CircoloCulturale.models.Socio;
 import it.unife.cavicchidome.CircoloCulturale.repositories.SocioRepository;
 import it.unife.cavicchidome.CircoloCulturale.services.SocioService;
+<<<<<<< Updated upstream
+=======
+import it.unife.cavicchidome.CircoloCulturale.services.TesseraService;
+import it.unife.cavicchidome.CircoloCulturale.services.UtenteService;
+import it.unife.cavicchidome.CircoloCulturale.models.Utente;
+import it.unife.cavicchidome.CircoloCulturale.models.Tessera;
+import it.unife.cavicchidome.CircoloCulturale.services.TesseraService;
+import it.unife.cavicchidome.CircoloCulturale.repositories.UtenteRepository;
+>>>>>>> Stashed changes
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
@@ -14,15 +23,41 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.Optional;
 
+<<<<<<< Updated upstream
+=======
+import org.apache.commons.validator.routines.EmailValidator;
+import org.springframework.format.annotation.DateTimeFormat;
+import java.util.regex.Pattern;
+
+>>>>>>> Stashed changes
 @Controller
 public class AuthController {
 
     private final SocioService socioService;
+    private final UtenteService utenteService;
     SocioRepository socioRepository;
+<<<<<<< Updated upstream
 
     AuthController(SocioRepository socioRepository, SocioService socioService) {
         this.socioRepository = socioRepository;
         this.socioService = socioService;
+=======
+    UtenteRepository utenteRepository;
+    TesseraService tesseraService;
+
+    AuthController(
+            SocioRepository socioRepository,
+            SocioService socioService,
+            UtenteRepository utenteRepository,
+            UtenteService utenteService,
+            TesseraService tesseraService
+    ){
+        this.socioRepository = socioRepository;
+        this.socioService = socioService;
+        this.utenteRepository = utenteRepository;
+        this.utenteService = utenteService;
+        this.tesseraService = tesseraService;
+>>>>>>> Stashed changes
     }
 
     @GetMapping("/login")
@@ -47,4 +82,77 @@ public class AuthController {
             return "redirect:/login";
         }
     }
+<<<<<<< Updated upstream
+=======
+
+    @GetMapping("/logout")
+    public String logout(
+            HttpServletRequest request,
+            HttpServletResponse response
+    ) {
+        // Invalida la sessione
+        //request.getSession().invalidate();
+
+        // Rimuove il cookie di autenticazione
+        Cookie socioCookie = new Cookie("socio-id", null);
+        socioCookie.setMaxAge(0);
+        response.addCookie(socioCookie);
+
+        // Reindirizza l'utente alla pagina di login
+        return "redirect:/login";
+    }
+
+    @GetMapping("/signup")
+    public String viewSignup() {
+        return "signup";
+    }
+
+
+
+    @PostMapping("/signup")
+    public String register(
+            @RequestParam String name,
+            @RequestParam String surname,
+            @RequestParam String cf,
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate dob,
+            @RequestParam String birthplace,
+            @RequestParam String state,
+            @RequestParam String province,
+            @RequestParam String city,
+            @RequestParam String street,
+            @RequestParam String houseNumber,
+            @RequestParam String email,
+            @RequestParam String password,
+            @RequestParam String phoneNumber,
+            @RequestParam String photoUrl,
+            RedirectAttributes redirectAttributes
+    ) {
+        // Valida i dati del form di registrazione
+        if (!(utenteService.validateUserInfo(name, surname, cf, dob, birthplace, state, province, city, street, houseNumber) &&
+                socioService.validateSocioInfo(email, password, phoneNumber, photoUrl))) {
+            redirectAttributes.addAttribute("failed", "true");
+            return "redirect:/signup";
+        }
+
+        // Registra utente se non presente nel Database
+        if(utenteRepository.findByCf(cf) == null){
+            Utente utente = utenteService.createUtente(name, surname, cf, dob, birthplace, state, province, city, street, houseNumber);
+            Socio socio = socioService.createSocio(utente, email, password, phoneNumber, photoUrl);
+            Tessera tessera = tesseraService.createTessera(socio);
+
+        }
+        else{
+            redirectAttributes.addAttribute("failed", "true");
+            redirectAttributes.addAttribute("alreadyPresent", "true");
+            return "redirect:/signup";
+        }
+        // Reindirizza l'utente alla pagina di login
+        return "redirect:/login";
+
+
+    }
+
+
+
+>>>>>>> Stashed changes
 }
--- src/main/java/it/unife/cavicchidome/CircoloCulturale/services/SocioService.java
+++ src/main/java/it/unife/cavicchidome/CircoloCulturale/services/SocioService.java
@@ -1,10 +1,13 @@
 package it.unife.cavicchidome.CircoloCulturale.services;
 
 import it.unife.cavicchidome.CircoloCulturale.models.Socio;
+import it.unife.cavicchidome.CircoloCulturale.models.Utente;
 import it.unife.cavicchidome.CircoloCulturale.repositories.SocioRepository;
+import org.apache.commons.validator.routines.EmailValidator;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.Instant;
 import java.util.Optional;
 
 @Service
@@ -25,4 +28,65 @@ public class SocioService {
             return Optional.empty();
         }
     }
+
+    public boolean validateSocioInfo(
+            String email,
+            String password,
+            String phoneNumber,
+            String photoUrl
+    ) {
+        // Crea un'istanza di EmailValidator
+        EmailValidator emailValidator = EmailValidator.getInstance();
+
+        // Controlla se l'email è un'email valida e non supera i 50 caratteri
+        if (email == null || email.length() > 50 || !emailValidator.isValid(email)) {
+            return false;
+        }
+
+        // Controlla se la password ha almeno 8 caratteri, almeno una lettera maiuscola, una lettera minuscola, un numero e non supera i 50 caratteri
+        if (password == null || password.length() > 50 || !password.matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{8,50}$")) {
+            return false;
+        }
+
+        // Controlla se il numero di telefono contiene solo numeri e ha esattamente 10 cifre
+        if (phoneNumber != null && !phoneNumber.isEmpty()){
+            if( !phoneNumber.matches("^[0-9]{10}$")) {
+                return false;
+            }
+        }
+
+        // Controlla se l'URL della foto è un URL valido e non supera gli 80 caratteri
+        if (photoUrl != null && !photoUrl.isEmpty()){
+            if (photoUrl.length() > 80 || !photoUrl.matches("^(ftp|http|https):\\/\\/[^ \"]+$")) {
+                return false;
+            }
+        }
+
+
+        // Se tutti i controlli passano, restituisce true
+        return true;
+    }
+
+    @Transactional
+    public Socio createSocio(
+            Utente utente,
+            String email,
+            String password,
+            String phoneNumber,
+            String photoUrl
+    ){
+        // Crea un nuovo socio
+        Socio socio = new Socio();
+        socio.setUtente(utente);
+        socio.setId(utente.getId());
+        socio.setEmail(email);
+        socio.setPassword(password);
+        socio.setTelefono(phoneNumber);
+        socio.setUrlFoto(photoUrl);
+
+
+        return socioRepository.save(socio);
+    }
+
+
 }
